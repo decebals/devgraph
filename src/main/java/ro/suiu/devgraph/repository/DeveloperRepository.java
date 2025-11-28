69c4ea18df2d257c8ee2efcf1f019a83a8afd356
Decebal Suiu <decebal.suiu@gmail.com>
2025-11-28 15:09:30 +0200

    Fix query existsByEmail

--- src/main/java/ro/suiu/devgraph/repository/DeveloperRepository.java
+++ src/main/java/ro/suiu/devgraph/repository/DeveloperRepository.java
@@ -1,14 +1,12 @@
 package ro.suiu.devgraph.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
 import ro.suiu.devgraph.domain.Developer;
 
 @Repository
 public interface DeveloperRepository extends Neo4jRepository<Developer, String> {
 
-    @Query("MATCH (d:Developer {email: $email}) RETURN d")
     boolean existsByEmail(String email);
 
 }
